b4370ab9788a3d6bdd9466d3b1f52f130a8d54af
Ivan Sorokin <unxed@mail.ru>
2022-01-23 05:02:53 +0300

    Copied sources from wide plugin version

---
+++ mb/build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+fpc HelloWorld.pas
---
+++ mb/install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo mkdir /usr/lib/far2l/Plugins/helloworld
+sudo cp ./libHelloWorld.so /usr/lib/far2l/Plugins/helloworld/HelloWorld.far-plug-wide
+sudo cp *.lng /usr/lib/far2l/Plugins/helloworld
+sudo cp *.hlf /usr/lib/far2l/Plugins/helloworld
